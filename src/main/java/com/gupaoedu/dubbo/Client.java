826d6c73fb3517903ea277023dc97dcb33c3404e
panning255 <panning255@jk.cn>
2020-03-09 20:09:26 +0800

    commit

--- src/main/java/com/gupaoedu/dubbo/Client.java
+++ src/main/java/com/gupaoedu/dubbo/Client.java
@@ -1,26 +1,16 @@
 package com.gupaoedu.dubbo;
 
-import com.alibaba.dubbo.common.extension.ExtensionLoader;
-import com.alibaba.dubbo.rpc.Protocol;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 
 public class Client {
-    public static void main(String[] args) throws IOException, InterruptedException {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("dubbo-client.xml");
-//
-//        //得到IGpHello的远程代理对象
-//        IGpHello iGpHello = (IGpHello) context.getBean("gpHelloService");
-//
-//        System.out.println(iGpHello.sayHello("Mic"));
-//        Thread.sleep(4000);
-//
-//        System.in.read();
-
-
-        Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getExtension("myProtocol");
-        System.out.println(protocol.getDefaultPort());
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("dubbo-client.xml");
+        //得到IGpHello的远程代理对象
+        IGpHello iGpHello = (IGpHello) context.getBean("gpHelloService");
+        System.out.println(iGpHello.sayHello("Mic"));
+        System.in.read();
     }
 }
